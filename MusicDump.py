495db9492c59957fa4e730d51182b0ca6ca32b31
Erin Brynn <erinbrynn@Erins-MacBook-Pro.local>
2019-04-09 16:23:44 -0400

    Grab data from spotify and start creating database

--- MusicDump.py
+++ MusicDump.py
@@ -1,1 +1,15 @@
-print("Dump music here")
+import requests
+import json
+import sqlite3
+import APIKeys
+import spotipy
+import spotipy.oauth2 as oauth2
+import MusicGrab
+
+conn = sqlite3.connect("spotify.sqlite")
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS SpotifyData (rating_spotify INTEGER, song TEXT")
+''' Using track_number (rating), name, create a date column of table that just has the day we collected it on,
+make separate columns for : rating_spotify, rating_itunes, date, song, rating_score'''
+
--- MusicGrab.py
+++ MusicGrab.py
@@ -4,12 +4,24 @@ import requests
 import json
 import sqlite3
 import APIKeys
+import spotipy
+import spotipy.oauth2 as oauth2
 
 ## API Keys
-client_id = APIKeys.client_id
-client_secret = APIKeys.client_secret 
+client_id_ = APIKeys.client_id
+client_secret_ = APIKeys.client_secret
+
+def generate_token():
+    credentials = oauth2.SpotifyClientCredentials(client_id= client_id_, client_secret= client_secret_)
+    token = credentials.get_access_token()
+    return token
 
 # Set up library to grab stuff from twitter with your authentication, and
 # return it in a JSON-formatted way
-url = 'https://api.spotify.com'
-params = {}
+
+token = generate_token()
+spotify = spotipy.Spotify(auth = token)
+results = spotify.user_playlist_tracks(user="spotifycharts", playlist_id="37i9dQZEVXbLRQDuF5jeBp") #large anount of data from top 50 list
+print(results['items'][0])
+    
+conn = sqlite3
